333c756b8e86966947e5454d7c3a6d7652ec1a47
Priscilla JOLY <priscilla.joly@epitech.eu>
2024-02-05 16:53:24 +0100

    UPDATE track route api

--- server/service/config.url.js
+++ server/service/config.url.js
@@ -2,10 +2,12 @@ const SPOTIFY_URL = "https://accounts.spotify.com/api/token";
 const SPOTIFY_URL_ALBUM = "https://api.spotify.com/v1/albums";
 const SPOTIFY_URL_ARTIST = "https://api.spotify.com/v1/artists";
 const SPOTIFY_URL_SEARCH = "https://api.spotify.com/v1/search?q=";
+const SPOTIFY_URL_TRACK = "https://api.spotify.com/v1/tracks";
 
 module.exports = {
     SPOTIFY_URL,
     SPOTIFY_URL_ALBUM,
     SPOTIFY_URL_ARTIST,
     SPOTIFY_URL_SEARCH,
+    SPOTIFY_URL_TRACK
 }
--- server/service/track.js
+++ server/service/track.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-const {SPOTIFY_URL_ALBUM} = require("./config.url");
+const {SPOTIFY_URL_TRACK} = require("./config.url");
 const {generate_token} = require("./spotify");
 
 const get_by_id = async (id) => {
     const token = await generate_token();
 
     try {
-        const response = await axios.get(`${SPOTIFY_URL_ALBUM}/${id}/tracks`, {
+        const response = await axios.get(`${SPOTIFY_URL_TRACK}/${id}`, {
             headers: {
                 'Authorization': `Bearer ${token.access_token}`
             }
