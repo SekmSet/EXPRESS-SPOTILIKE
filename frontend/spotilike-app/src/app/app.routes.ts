ae4e7697e8066759d5238bedaf44d113a9ec17fe
Priscilla JOLY <priscilla.joly@epitech.eu>
2024-02-05 11:08:55 +0100

    Add auth guard

--- frontend/spotilike-app/src/app/app.routes.ts
+++ frontend/spotilike-app/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { ListAlbumsComponent } from './list-albums/list-albums.component';
 import { DetailsAlbumComponent } from './displays/details-album/details-album.component';
 import { LoginComponent } from './login/login.component';
+import {userAuthGuard} from "../guard/user-auth.guard";
 
 export const routes: Routes = [
 {path: '', component: LoginComponent},
-{path: 'albums',  component: ListAlbumsComponent},
-{path: 'album-details', component: DetailsAlbumComponent }
+{path: 'albums',  component: ListAlbumsComponent, canActivate: [userAuthGuard]},
+{path: 'album-details', component: DetailsAlbumComponent, canActivate: [userAuthGuard] }
 ];
